ef809e511d74a368370b7f14012680e4a72bad0e
Dargaz <dargazwork@gmail.com>
2024-08-02 05:14:09 +0300

    Изменения за 13.07

--- js/script.js
+++ js/script.js
@@ -7,3 +7,28 @@ function plusesClick(hide_text) {
         else text.style.display = "block";
     }
 }
+
+let form = document.querySelector('.myForm');
+    formInputs = form.querySelectorAll('.input');
+    inputName = form.querySelector('.nameText');
+    inputEmail = form.querySelector('.emailText');
+
+
+
+form.onsubmit = function(){
+    nameVal = inputName.value;
+    emailVal = inputEmail.value;
+
+
+    formInputs.forEach(function (input){
+        if (input.value === ''){
+            input.classList.add('error');
+        } else {
+            input.classList.remove('error');
+        }
+    })
+return false;
+}
+
+
+
